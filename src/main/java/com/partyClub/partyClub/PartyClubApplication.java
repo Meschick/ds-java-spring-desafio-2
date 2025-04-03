810f2118ac131545e96f265154f18467dd473e21
Kawan Passos Meschick <kawanpassos5000@gmail.com>
2025-04-02 22:42:03 -0300

    feat: cria estrutura base do projeto

---
+++ src/main/java/com/partyClub/partyClub/PartyClubApplication.java
@@ -0,0 +1,13 @@
+package com.partyClub.partyClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyClubApplication.class, args);
+	}
+
+}
